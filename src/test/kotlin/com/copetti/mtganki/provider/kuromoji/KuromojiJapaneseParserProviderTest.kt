a5ba91a7437a7bcc93319a7e3a9cea599354792e
Luís Copetti <luis@mana.app>
2024-10-27 19:13:45 -0300

    Fix test file name

--- src/test/kotlin/com/copetti/mtganki/provider/kuromoji/KuromojiJapaneseParserProviderTest.kt
+++ src/test/kotlin/com/copetti/mtganki/provider/kuromoji/KuromojiJapaneseParserProviderTest.kt
@@ -5,7 +5,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class KuromojiJishoJapaneseParserProviderTest {
+class KuromojiJapaneseParserProviderTest {
 
     private lateinit var kuromojiJapaneseParserProvider: KuromojiJapaneseParserProvider
 
