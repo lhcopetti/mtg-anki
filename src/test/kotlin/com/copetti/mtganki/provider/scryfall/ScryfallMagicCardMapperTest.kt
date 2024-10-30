adf53b4cf09427ee3bde93fe71d791e88e208d4e
Luís Copetti <lhcopetti@gmail.com>
2024-10-29 23:04:59 -0300

    Fix DualLanguageText class so that original always equals to english

--- src/test/kotlin/com/copetti/mtganki/provider/scryfall/ScryfallMagicCardMapperTest.kt
+++ src/test/kotlin/com/copetti/mtganki/provider/scryfall/ScryfallMagicCardMapperTest.kt
@@ -27,12 +27,12 @@ class ScryfallMagicCardMapperTest {
             set = "dsk",
             lang = "ja",
             names = listOf(
-                DualLanguageText(original = "最後の復讐", translation = "Final Vengeance")
+                DualLanguageText(original = "Final Vengeance", translation = "最後の復讐")
             ),
             texts = listOf(
                 DualLanguageText(
-                    original = "この呪文を唱えるための追加コストとして、エンチャントやクリーチャーである１つを生け贄に捧げる。\nクリーチャー１体を対象とする。それを追放する。",
-                    translation = "As an additional cost to cast this spell, sacrifice a creature or enchantment.\nExile target creature."
+                    original = "As an additional cost to cast this spell, sacrifice a creature or enchantment.\nExile target creature.",
+                    translation = "この呪文を唱えるための追加コストとして、エンチャントやクリーチャーである１つを生け贄に捧げる。\nクリーチャー１体を対象とする。それを追放する。",
                 )
             )
         )
@@ -49,17 +49,17 @@ class ScryfallMagicCardMapperTest {
             set = "dsk",
             lang = "ja",
             names = listOf(
-                DualLanguageText(original = "Meat Locker // Drowned Diner", translation = "Meat Locker"),
-                DualLanguageText(original = "Meat Locker // Drowned Diner", translation = "Drowned Diner"),
+                DualLanguageText(original = "Meat Locker", translation = "Meat Locker // Drowned Diner"),
+                DualLanguageText(original = "Drowned Diner", translation = "Meat Locker // Drowned Diner"),
             ),
             texts = listOf(
                 DualLanguageText(
-                    original = "あなたがこのドアを開放したとき、クリーチャー最大１体を対象とする。それをタップし、それの上に麻痺カウンター２個を置く。（麻痺カウンターが置かれているパーマネントがアンタップ状態になるなら、代わりにそれの上から麻痺カウンター１個を取り除く。）\n（どちらの半分でも唱えることができる。そのドアは戦場で開放される。閉鎖されているドアのマナ・コストを支払って、それを開放してもよい。これはソーサリーとして行う。）",
-                    translation = "When you unlock this door, tap up to one target creature and put two stun counters on it. (If a permanent with a stun counter would become untapped, remove one from it instead.)\n(You may cast either half. That door unlocks on the battlefield. As a sorcery, you may pay the mana cost of a locked door to unlock it.)"
+                    original = "When you unlock this door, tap up to one target creature and put two stun counters on it. (If a permanent with a stun counter would become untapped, remove one from it instead.)\n(You may cast either half. That door unlocks on the battlefield. As a sorcery, you may pay the mana cost of a locked door to unlock it.)",
+                    translation = "あなたがこのドアを開放したとき、クリーチャー最大１体を対象とする。それをタップし、それの上に麻痺カウンター２個を置く。（麻痺カウンターが置かれているパーマネントがアンタップ状態になるなら、代わりにそれの上から麻痺カウンター１個を取り除く。）\n（どちらの半分でも唱えることができる。そのドアは戦場で開放される。閉鎖されているドアのマナ・コストを支払って、それを開放してもよい。これはソーサリーとして行う。）",
                 ),
                 DualLanguageText(
-                    original = "冠（かん）水（すい）した食（しょく）堂（どう）\no3oUoU\nあなたがこのドアを開放したとき、カード３枚を引き、その後、カード１枚を捨てる。",
-                    translation = "When you unlock this door, draw three cards, then discard a card.\n(You may cast either half. That door unlocks on the battlefield. As a sorcery, you may pay the mana cost of a locked door to unlock it.)"
+                    original = "When you unlock this door, draw three cards, then discard a card.\n(You may cast either half. That door unlocks on the battlefield. As a sorcery, you may pay the mana cost of a locked door to unlock it.)",
+                    translation = "冠（かん）水（すい）した食（しょく）堂（どう）\no3oUoU\nあなたがこのドアを開放したとき、カード３枚を引き、その後、カード１枚を捨てる。",
                 )
             )
         )
