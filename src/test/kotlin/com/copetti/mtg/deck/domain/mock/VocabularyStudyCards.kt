4839088ec2c5c17353eb0f790790518ef8005fa6
Luís Copetti <lhcopetti@gmail.com>
2024-11-09 18:51:32 -0300

    Add set information to cards

--- src/test/kotlin/com/copetti/mtg/deck/domain/mock/VocabularyStudyCards.kt
+++ src/test/kotlin/com/copetti/mtg/deck/domain/mock/VocabularyStudyCards.kt
@@ -1,15 +1,18 @@
 package com.copetti.mtg.deck.domain.mock
 
+import com.copetti.mtg.deck.domain.model.MagicSet
 import com.copetti.mtg.deck.domain.model.VocabularyStudyCard
 
 object VocabularyStudyCards {
 
-    fun givenVocabularyStudyCard() = VocabularyStudyCard(
+    fun givenVocabularyStudyCard(
+        sets: Set<MagicSet> = setOf(MagicSets.givenMagicSet())
+    ) = VocabularyStudyCard(
         vocabulary = "the-vocabulary",
         definition = VocabularyDefinitions.givenVocabularyDefinition(),
         cards = setOf(
             MagicCards.givenSingleFacedCard()
         ),
-        sets = setOf(MagicSets.givenMagicSet())
+        sets = sets
     )
 }
