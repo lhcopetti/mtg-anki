718ad1862b7972a8e3237a9cb6a88e4272acbdfc
Luís Copetti <luis@mana.app>
2024-11-11 21:50:49 -0300

    Remove vocabularies that are just color combinations

--- src/main/kotlin/com/copetti/mtg/deck/domain/usecase/ProcessParsedVocabulary.kt
+++ src/main/kotlin/com/copetti/mtg/deck/domain/usecase/ProcessParsedVocabulary.kt
@@ -7,10 +7,17 @@ class ProcessParsedVocabulary {
 
     fun process(vocabularies: Set<String>): Set<String> {
         return vocabularies
-            .filter { vocabulary -> containsKanji(vocabulary) }
+            .filter(this::containsKanji)
+            .filterNot(this::combinationOfColors)
             .toSet()
     }
 
+    private fun combinationOfColors(vocabulary: String) = vocabulary.length > 1 && vocabulary.all { c -> c in COLORS }
+
     private fun containsKanji(vocabulary: String) =
         vocabulary.any { c -> Character.UnicodeBlock.of(c) == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS }
+
+    companion object {
+        private const val COLORS = "白青黒赤緑"
+    }
 }
--- src/test/kotlin/com/copetti/mtg/deck/domain/usecase/ProcessParsedVocabularyTest.kt
+++ src/test/kotlin/com/copetti/mtg/deck/domain/usecase/ProcessParsedVocabularyTest.kt
@@ -30,4 +30,47 @@ class ProcessParsedVocabularyTest {
         assertThat(actual).isEqualTo(expected)
     }
 
+    @Test
+    fun `should keep individual color vocabulary`() {
+
+        val input = setOf(
+            "白",
+            "青",
+            "黒",
+            "赤",
+            "緑",
+        )
+        val actual = processParsedVocabulary.process(input)
+        val expected = setOf(
+            "白",
+            "青",
+            "黒",
+            "赤",
+            "緑"
+        )
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should remove color combinations`() {
+
+        val input = setOf(
+            "黒赤",
+            "黒赤緑",
+            "青",
+            "白青赤緑",
+            "白青黒赤緑",
+            "黒",
+            "白青",
+            "青黒",
+            "赤緑",
+            "黒赤青",
+        )
+        val actual = processParsedVocabulary.process(input)
+        val expected = setOf(
+            "青",
+            "黒",
+        )
+        assertThat(actual).isEqualTo(expected)
+    }
 }
