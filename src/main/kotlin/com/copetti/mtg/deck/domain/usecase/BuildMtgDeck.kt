c539ea164935ca4c07ca92b31f9c085320501fa2
Luís Copetti <luis@mana.app>
2024-11-16 12:15:40 -0300

    Added some helpful logs

--- src/main/kotlin/com/copetti/mtg/deck/domain/usecase/BuildMtgDeck.kt
+++ src/main/kotlin/com/copetti/mtg/deck/domain/usecase/BuildMtgDeck.kt
@@ -1,10 +1,14 @@
 package com.copetti.mtg.deck.domain.usecase
 
+import com.copetti.mtg.deck.common.extensions.getLogger
 import com.copetti.mtg.deck.domain.model.FlashCard
 import com.copetti.mtg.deck.domain.model.VocabularyStudyCard
 import com.copetti.mtg.deck.gateway.CreateDeckProvider
 import com.copetti.mtg.deck.gateway.CreateDeckProviderRequest
 import org.springframework.stereotype.Component
+import java.nio.file.Files
+import java.time.LocalDateTime
+import kotlin.io.path.Path
 
 @Component
 class BuildMtgDeck(
@@ -14,11 +18,18 @@ class BuildMtgDeck(
     val createDeckProvider: CreateDeckProvider
 ) {
 
+    private val log = getLogger()
+
     fun buildDeck(exportFilePath: String) {
+        log.info("Beginning construction of magic deck | outputFilePath: $exportFilePath")
         val magicData = loadMagicData.load()
+        log.info("Magic data loaded successfully | cards: #${magicData.cards.size}, sets: #${magicData.sets.size}")
         val studyCards = processMagicData.process(magicData)
+        log.info("Study cards created successfully | studyCards: #${studyCards.size}")
         val flashCardEntries = createFlashCards(studyCards)
+        log.info("Flash card creation completed")
         createDeckProvider.create(CreateDeckProviderRequest(exportFilePath, flashCardEntries))
+        log.info("Deck created successfully | outputFilePath: $exportFilePath")
     }
 
     private fun createFlashCards(studyCard: Set<VocabularyStudyCard>): List<FlashCard> {
--- src/main/kotlin/com/copetti/mtg/deck/provider/scryfall/ScryfallMagicCardsProvider.kt
+++ src/main/kotlin/com/copetti/mtg/deck/provider/scryfall/ScryfallMagicCardsProvider.kt
@@ -1,9 +1,12 @@
 package com.copetti.mtg.deck.provider.scryfall
 
+import com.copetti.mtg.deck.common.extensions.getLogger
 import com.copetti.mtg.deck.domain.model.MagicCard
 import com.copetti.mtg.deck.gateway.MagicCardsProvider
-import com.copetti.mtg.deck.provider.scryfall.client.ScryfallBulkDataClient
 import com.copetti.mtg.deck.provider.scryfall.client.ScryfallApiClient
+import com.copetti.mtg.deck.provider.scryfall.client.ScryfallBulkDataClient
+import com.copetti.mtg.deck.provider.scryfall.model.ScryfallBulkData
+import com.copetti.mtg.deck.provider.scryfall.model.ScryfallBulkDataResponse
 import org.springframework.stereotype.Component
 import java.util.stream.Stream
 
@@ -14,6 +17,8 @@ class ScryfallMagicCardsProvider(
     private val mapper: ScryfallCardMapper
 ) : MagicCardsProvider {
 
+    private val log = getLogger()
+
     override fun loadAll(): Stream<MagicCard> {
 
         val bulkDataDownloadUri =
@@ -23,9 +28,16 @@ class ScryfallMagicCardsProvider(
             .map(mapper::toMagicCard)
     }
 
-    private fun retrieveDownloadUrl() = client.retrieveBulkDataCatalog()
-        .data.firstOrNull { data -> data.type == BULK_DATA_ALL_CARDS_TYPE }
-        ?.downloadUri
+    private fun retrieveDownloadUrl(): String? {
+        log.info("Retrieving Scryfall bulk data catalog")
+        return client.retrieveBulkDataCatalog()
+            .also { log.info("Retrieved data catalog successfully | dataTypes: ${getAllDataTypes(it)}") }
+            .data.firstOrNull { data -> data.type == BULK_DATA_ALL_CARDS_TYPE }
+            ?.downloadUri
+    }
+
+    private fun getAllDataTypes(response: ScryfallBulkDataResponse) =
+        response.data.joinToString(separator = ", ", transform = ScryfallBulkData::type)
 
     companion object {
         private const val BULK_DATA_ALL_CARDS_TYPE = "all_cards"
